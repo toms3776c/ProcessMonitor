5812a40020beb266e4a76fc9edacf0afa2332ea7
toms3776c <nagaya.develop@gmail.com>
2019-03-04 23:16:38 +0900

    2nd Commit

--- ProcessMonitor/Program.cs
+++ ProcessMonitor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace ProcessMonitor
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                bool flg = false;
+                Process[] processList = Process.GetProcesses();
+                foreach (Process p in processList)
+                {
+                    if (p.ProcessName == "EXCEL")
+                    {
+                        flg = true;
+                        if (p.Responding)
+                        {
+                            
+                            Console.WriteLine("OK");
+                            break;
+                        }
+                        else
+                        {
+                            p.CloseMainWindow();
+                            //RunApp();
+                        }
+                    }
+                }
+                if (!flg)
+                {
+                    //RunApp();
+                }
+                System.Threading.Thread.Sleep(5000);
+            }
         }
     }
 }
